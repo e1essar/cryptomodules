24e95d9d0c3b06124eb925c81cc32e9eead713fb
e1essar <80064778+e1essar@users.noreply.github.com>
2023-12-09 22:33:25 +0700

    upd: aes

--- aes-module/aes.cpp
+++ aes-module/aes.cpp
@@ -2,13 +2,53 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/aes.h>
+#include <openssl/rand.h>
+#include <iostream>
 #include <string.h>
+
+struct EncryptionParameters {
+    unsigned char key[AES_BLOCK_SIZE];
+    unsigned char iv[AES_BLOCK_SIZE];
+
+    EncryptionParameters() {
+        memset(key, 0, AES_BLOCK_SIZE);
+        memset(iv, 0, AES_BLOCK_SIZE);
+    }
+
+    EncryptionParameters(const unsigned char* newKey, const unsigned char* newIV) {
+        memcpy(key, newKey, AES_BLOCK_SIZE);
+        memcpy(iv, newIV, AES_BLOCK_SIZE);
+    }
+};
+
+EncryptionParameters globalEncryptionParams;
 
 class AESEncryption {
 public:
+    AESEncryption() {
+        generateRandomKey(key, AES_BLOCK_SIZE);
+        generateRandomIV(iv, AES_BLOCK_SIZE);
+
+        globalEncryptionParams = EncryptionParameters(key, iv);
+    }
+
+    AESEncryption(bool det, unsigned char detIV = 0) {
+        if (det) {
+            generateRandomKey(key, AES_BLOCK_SIZE);
+            memset(iv, detIV, AES_BLOCK_SIZE);
+        } else {
+            generateRandomKey(key, AES_BLOCK_SIZE);
+            generateRandomIV(iv, AES_BLOCK_SIZE);
+        }
+
+        globalEncryptionParams = EncryptionParameters(key, iv);
+    }
+
     AESEncryption(const unsigned char* key, const unsigned char* iv) {
         memcpy(this->key, key, AES_BLOCK_SIZE);
         memcpy(this->iv, iv, AES_BLOCK_SIZE);
+
+        globalEncryptionParams = EncryptionParameters(key, iv);
     }
 
     ~AESEncryption() {}
@@ -67,6 +107,18 @@ public:
         return plaintext_len;
     }
 
+    void generateRandomKey(unsigned char* key, size_t size) {
+        if (RAND_bytes(key, static_cast<int>(size)) != 1) {
+            handleErrors();
+        }
+    }
+
+    void generateRandomIV(unsigned char* iv, size_t size) {
+        if (RAND_bytes(iv, static_cast<int>(size)) != 1) {
+            handleErrors();
+        }
+    }
+
 private:
     void handleErrors() {
         ERR_print_errors_fp(stderr);
@@ -79,33 +131,67 @@ private:
     int plaintext_len;
 };
 
-int main(void) {
-    // Key and IV
-    unsigned char key[] = "0123456789abcdef";
-    unsigned char iv[] = "1234567887654321";
-    unsigned char plaintext[] = "Advanced Encryption Standard(AES) is a symmetric encryption algorithm";
-    
-    // Buffer for ciphertext
+void testRandom() {
+    AESEncryption aesRandomEncryption;
+
+    const char* plaintext = "Hello, World!";
     unsigned char ciphertext[128];
-    // Buffer for the decrypted text
-    unsigned char decryptedtext[128];
 
-    AESEncryption aes(key, iv);
-    aes.encrypt(plaintext, strlen((char*)plaintext), ciphertext);
+    aesRandomEncryption.encrypt(reinterpret_cast<const unsigned char*>(plaintext), strlen(plaintext), ciphertext);
 
-    printf("Ciphertext is:\n");
-    BIO_dump_fp(stdout, (const char*)ciphertext, aes.getCiphertextLen());
-
-    // HEX
-    char hexString[2 * aes.getCiphertextLen() + 1];
-    aes.ucharToHex(ciphertext, aes.getCiphertextLen(), hexString);
+    char hexString[2 * aesRandomEncryption.getCiphertextLen() + 1];
+    aesRandomEncryption.ucharToHex(ciphertext, aesRandomEncryption.getCiphertextLen(), hexString);
     printf("Ciphertext in Hex:\n%s\n", hexString);
 
-    aes.decrypt(ciphertext, aes.getCiphertextLen(), decryptedtext);
-    decryptedtext[aes.getPlaintextLen()] = '\0';
+    std::cout << "Key: ";
+    for (int i = 0; i < AES_BLOCK_SIZE; ++i) {
+        printf("%02x", globalEncryptionParams.key[i]);
+    }
+    std::cout << std::endl;
+
+    std::cout << "IV: ";
+    for (int i = 0; i < AES_BLOCK_SIZE; ++i) {
+        printf("%02x", globalEncryptionParams.iv[i]);
+    }
+    std::cout << std::endl;
+
+    unsigned char decryptedtext[128];
+    AESEncryption aesDecryption(globalEncryptionParams.key, globalEncryptionParams.iv);
+    aesDecryption.decrypt(ciphertext, aesRandomEncryption.getCiphertextLen(), decryptedtext);
+    decryptedtext[aesDecryption.getPlaintextLen()] = '\0';
 
     printf("Decrypted text is:\n");
     printf("%s\n", decryptedtext);
+}
 
+void testDeterministic() {
+    const char* plaintext = "Hello, World!";
+    unsigned char ciphertext1[128];
+    unsigned char ciphertext2[128];
+
+    AESEncryption aesDeterministic1(true);
+    AESEncryption aesDeterministic2(globalEncryptionParams.key, globalEncryptionParams.iv);
+
+    aesDeterministic1.encrypt(reinterpret_cast<const unsigned char*>(plaintext), strlen(plaintext), ciphertext1);
+    aesDeterministic2.encrypt(reinterpret_cast<const unsigned char*>(plaintext), strlen(plaintext), ciphertext2);
+
+    char hexString1[2 * aesDeterministic1.getCiphertextLen() + 1];
+    aesDeterministic1.ucharToHex(ciphertext1, aesDeterministic1.getCiphertextLen(), hexString1);
+    printf("Ciphertext 1 in Hex:\n%s\n", hexString1);
+
+    char hexString2[2 * aesDeterministic2.getCiphertextLen() + 1];
+    aesDeterministic2.ucharToHex(ciphertext2, aesDeterministic2.getCiphertextLen(), hexString2);
+    printf("Ciphertext 2 in Hex:\n%s\n", hexString2);
+
+    if (memcmp(ciphertext1, ciphertext2, std::min(aesDeterministic1.getCiphertextLen(), aesDeterministic2.getCiphertextLen())) == 0) {
+        std::cout << "Ciphertexts match!" << std::endl;
+    } else {
+        std::cout << "Ciphertexts do not match!" << std::endl;
+    }
+}
+
+int main() {
+    testRandom();
+    testDeterministic();
     return 0;
 }
