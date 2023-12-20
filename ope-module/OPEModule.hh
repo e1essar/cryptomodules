6c58e807ec8cb989e7724c0d9c48cf35f9d4cc1e
e1essar <80064778+e1essar@users.noreply.github.com>
2023-12-21 04:02:19 +0700

    add: OPE class template

--- ope-module/OPEModule.hh
+++ ope-module/OPEModule.hh
@@ -1,1 +1,30 @@
+#pragma once
 
+#include<iostream>
+#include <vector>
+#include <random>
+#include <NTL/ZZ.h>
+#include "lib/ope.hh"
+
+using namespace std;
+
+class OPEModule {
+public:
+    OPEModule(const std::string &key = "default_key", size_t plainbits = 32, size_t cipherbits = 64);
+    ~OPEModule();
+    
+    typedef int INPUT_TYPE;
+    typedef int OUTPUT_TYPE;
+
+    typedef std::string KEY_TYPE;
+    typedef int DEFAULT_TYPE;
+    using TEST_TYPE = NTL::ZZ;
+
+    TEST_TYPE encrypt(INPUT_TYPE input, KEY_TYPE key);
+    TEST_TYPE decrypt(TEST_TYPE input, KEY_TYPE key);
+
+    KEY_TYPE generateKey();
+
+private:
+    OPE opeInstance;
+};
