06404fde8b167542076cfbdf313c044cb9eb7bda
e1essar <80064778+e1essar@users.noreply.github.com>
2023-12-21 04:03:39 +0700

    add: simple OPE tests

--- ope-module/OPEMain.cc
+++ ope-module/OPEMain.cc
@@ -1,1 +1,33 @@
+#include "OPEModule.hh"
+#include "OPEModule.cc"
 
+using namespace std;
+
+int main() {
+    OPEModule opeModule;
+
+    OPEModule::KEY_TYPE key = opeModule.generateKey();
+    int a = 45;
+    int b = 46;
+
+    OPEModule::TEST_TYPE enc1 = opeModule.encrypt(a, key);
+    OPEModule::TEST_TYPE enc2 = opeModule.encrypt(b, key);
+
+    OPEModule::TEST_TYPE dec1 = opeModule.decrypt(enc1, key);
+    OPEModule::TEST_TYPE dec2 = opeModule.decrypt(enc2, key);
+
+    if(enc1 < enc2) {
+        cout << "OK" << endl;
+    }
+
+    if(a == dec1) {
+        cout << "OK" << endl;
+    }
+
+    if(b == dec2) {
+        cout << "OK" << endl;
+    }
+
+
+    return 0;
+}
